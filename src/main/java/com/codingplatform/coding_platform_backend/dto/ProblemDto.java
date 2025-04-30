eef3964b209ca90021ae3f1f771661605080ff8a
Duy Mach <mach.tom99@gmail.com>
2025-04-30 19:01:00 -0400

    Updated ProblemDto and ProblemMapper to include fucntionName, constraints, hints, isPremium, updatedAt, and visibility (V2 schema changes)

--- src/main/java/com/codingplatform/coding_platform_backend/dto/ProblemDto.java
+++ src/main/java/com/codingplatform/coding_platform_backend/dto/ProblemDto.java
@@ -1,11 +1,13 @@
 package com.codingplatform.coding_platform_backend.dto;
 
 import com.codingplatform.coding_platform_backend.models.enums.Difficulty;
+import com.codingplatform.coding_platform_backend.models.enums.ProblemVisibility;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 
 import java.time.LocalDateTime;
+import java.util.List;
 
 
 @Data
@@ -17,4 +19,10 @@ public class ProblemDto {
     private String description;
     private Difficulty difficulty;
     private LocalDateTime creationDate;
+    private String functionName;
+    private String constraints;
+    private List<String> hints;
+    private boolean isPremium;
+    private LocalDateTime updatedAt;
+    private ProblemVisibility visibility;
 }

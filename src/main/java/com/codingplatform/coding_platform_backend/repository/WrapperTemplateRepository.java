bff75a850b31eabb158d1a4877f836fb4764e3c1
Duy Mach <mach.tom99@gmail.com>
2025-05-10 17:42:23 -0400

    Updated findByProblemIdAndLanguage to return Optional of WrapperTemplate

--- src/main/java/com/codingplatform/coding_platform_backend/repository/WrapperTemplateRepository.java
+++ src/main/java/com/codingplatform/coding_platform_backend/repository/WrapperTemplateRepository.java
@@ -5,8 +5,10 @@ import com.codingplatform.coding_platform_backend.models.enums.LanguageName;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface WrapperTemplateRepository extends JpaRepository<WrapperTemplate, Long> {
     boolean existsByProblemIdAndLanguage(Long problemId, LanguageName language);
-    WrapperTemplate findByProblemIdAndLanguage(Long problemId, LanguageName language);
+    Optional<WrapperTemplate> findByProblemIdAndLanguage(Long problemId, LanguageName language);
 }

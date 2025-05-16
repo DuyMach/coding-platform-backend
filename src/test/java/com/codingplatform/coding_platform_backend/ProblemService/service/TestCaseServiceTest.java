0f9dc5e6e38df6fcbdef99bb5fb2f2488383acbe
Duy Mach <mach.tom99@gmail.com>
2025-05-16 08:47:42 -0400

    test: add unit test for updateTestCaseById and deletTestCase method inside of TestCaseServiceImpl

--- src/test/java/com/codingplatform/coding_platform_backend/ProblemService/service/TestCaseServiceTest.java
+++ src/test/java/com/codingplatform/coding_platform_backend/ProblemService/service/TestCaseServiceTest.java
@@ -1,8 +1,10 @@
 package com.codingplatform.coding_platform_backend.ProblemService.service;
 
 import com.codingplatform.coding_platform_backend.dto.TestCaseDto;
+import com.codingplatform.coding_platform_backend.dto.UpdateTestCaseDto;
 import com.codingplatform.coding_platform_backend.dto.mapper.TestCaseMapper;
 import com.codingplatform.coding_platform_backend.exception.ProblemNotFoundException;
+import com.codingplatform.coding_platform_backend.exception.TestCaseNotFoundException;
 import com.codingplatform.coding_platform_backend.models.Problem;
 import com.codingplatform.coding_platform_backend.models.TestCase;
 import com.codingplatform.coding_platform_backend.models.enums.Difficulty;
@@ -14,6 +16,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.ArgumentCaptor;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
@@ -21,8 +24,8 @@ import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Optional;
 import java.util.Set;
-import java.util.stream.Collectors;
 
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -36,13 +39,14 @@ public class TestCaseServiceTest {
     @InjectMocks
     private TestCaseServiceImpl testCaseService;
 
-    Problem problem;
+    private Problem problem;
 
-    TestCase testCase;
+    private TestCase testCase;
 
-    TestCaseDto testCaseDto;
+    private TestCaseDto testCaseDto;
 
-    Long problemId = 1L;
+    private final Long problemId = 1L;
+
 
     @BeforeEach
     void beforeEach() {
@@ -91,9 +95,10 @@ public class TestCaseServiceTest {
         when(problemRepository.findById(problemId)).thenReturn(Optional.empty());
 
         // Act and Assert
+        String PROBLEM_NOT_FOUND_ERROR = "Problem with given ID doesn't exist: ";
         Assertions.assertThatThrownBy(() -> testCaseService.createTestCase(problemId, testCaseDto))
                 .isInstanceOf(ProblemNotFoundException.class)
-                .hasMessage("Problem with given ID doesn't exist: " + problemId);
+                .hasMessage(PROBLEM_NOT_FOUND_ERROR + problemId);
     }
 
     @Test
@@ -127,13 +132,204 @@ public class TestCaseServiceTest {
     }
 
     @Test
+    public void TestCaseService_GetAllTestCasesByProblemId_ReturnsEmptySet_WhenNoTestCaseExist(){
+        // Arrange
+        Long problemId = 1L;
+
+        when(problemRepository.existsById(problemId)).thenReturn(true);
+        when(testCaseRepository.findAllByProblemId(problemId)).thenReturn(Set.of());
+
+        // Act
+        Set<TestCaseDto> result = testCaseService.getAllTestCasesByProblemId(problemId);
+
+        // Assert
+        Assertions.assertThat(result).isEmpty();
+    }
+
+    @Test
     public void TestCaseService_GetAllTestCasesByProblemId_ThrowsProblemNotFoundException_WhenProblemNotFound(){
         // Arrange
         when(problemRepository.existsById(problemId)).thenReturn(false);
 
         // Act and Assert
+        String PROBLEM_NOT_FOUND_ERROR = "Problem with given ID doesn't exist: ";
         Assertions.assertThatThrownBy(() -> testCaseService.getAllTestCasesByProblemId(problemId))
                 .isInstanceOf(ProblemNotFoundException.class)
-                .hasMessage("Problem with given ID doesn't exist: " + problemId);
+                .hasMessage(PROBLEM_NOT_FOUND_ERROR + problemId);
+    }
+
+    @Test
+    public void TestCaseService_UpdateTestCaseById_ReturnsUpdatedTestCaseDto(){
+        // Arrange
+        Long testCaseId = 1L;
+        UpdateTestCaseDto updateTestCaseDto = UpdateTestCaseDto.builder()
+                .input("updated test case input")
+                .expectedOutput("nothing")
+                .isSample(false)
+                .build();
+
+        TestCase updatedTestCase = TestCase.builder()
+                .input("updated test case input")
+                .expectedOutput("nothing")
+                .isSample(false)
+                .problem(problem)
+                .build();
+
+        when(testCaseRepository.findById(testCaseId)).thenReturn(Optional.of(testCase));
+        when(testCaseRepository.save(Mockito.any(TestCase.class))).thenReturn(updatedTestCase);
+
+        // Act
+        TestCaseDto result = testCaseService.updateTestCaseById(testCaseId, updateTestCaseDto);
+
+        // Assert
+        Assertions.assertThat(result).isNotNull();
+        Assertions.assertThat(result.getExpectedOutput()).isEqualTo(updateTestCaseDto.getExpectedOutput());
+        Assertions.assertThat(result.getIsSample()).isFalse();
+    }
+
+    @Test
+    public void TestCaseService_UpdateTestCaseById_SavesCorrectUpdatedEntity(){
+        // Arrange
+        Long testCaseId = 1L;
+        UpdateTestCaseDto updateTestCaseDto = UpdateTestCaseDto.builder()
+                .input("updated test case input")
+                .expectedOutput("nothing")
+                .isSample(false)
+                .build();
+
+        when(testCaseRepository.findById(testCaseId)).thenReturn(Optional.of(testCase));
+        when(testCaseRepository.save(Mockito.any(TestCase.class))).thenReturn(testCase); // return something to avoid null
+
+        // Act
+        TestCaseDto result = testCaseService.updateTestCaseById(testCaseId, updateTestCaseDto);
+
+        // Assert
+        ArgumentCaptor<TestCase> captor = ArgumentCaptor.forClass(TestCase.class);
+        verify(testCaseRepository).save(captor.capture());
+
+        TestCase captured = captor.getValue();
+        Assertions.assertThat(captured.getExpectedOutput()).isEqualTo(updateTestCaseDto.getExpectedOutput());
+        Assertions.assertThat(captured.getInput()).isEqualTo(updateTestCaseDto.getInput());
+        Assertions.assertThat(captured.isSample()).isFalse();
+        Assertions.assertThat(captured.getProblem()).isEqualTo(testCase.getProblem());
+    }
+
+    @Test
+    public void TestCaseService_UpdateTestCaseById_ThrowsTestCaseNotFoundException_WhenNotFound(){
+        // Arrange
+        Long testCaseId = 1L;
+        UpdateTestCaseDto updateTestCaseDto = UpdateTestCaseDto.builder()
+                .input("updated test case input")
+                .expectedOutput("nothing")
+                .isSample(false)
+                .build();
+
+        when(testCaseRepository.findById(testCaseId)).thenReturn(Optional.empty());
+
+        // Act and Assert
+        String TESTCASE_NOT_FOUND_ERROR = "Test Case with given ID doesn't exist: ";
+        Assertions.assertThatThrownBy(() -> testCaseService.updateTestCaseById(testCaseId, updateTestCaseDto))
+                .isInstanceOf(TestCaseNotFoundException.class)
+                .hasMessage(TESTCASE_NOT_FOUND_ERROR + testCaseId);
+    }
+
+    @Test
+    public void TestCaseService_UpdateTestCaseById_ThrowsIllegalArgumentException_WhenInputIsBlank(){
+        // Arrange
+        Long testCaseId = 1L;
+        UpdateTestCaseDto updateTestCaseDto = UpdateTestCaseDto.builder()
+                .input(" ") // blank input field
+                .expectedOutput("nothing")
+                .isSample(false)
+                .build();
+
+        when(testCaseRepository.findById(testCaseId)).thenReturn(Optional.of(testCase));
+
+        // Act and Assert
+        String EMPTY_INPUT_ERROR = "Cannot set the input field to nothing! " +
+                "If you do not wish to update the input field, " +
+                "please remove it from the JSON request. Only include what you want to update.";
+        Assertions.assertThatThrownBy(() -> testCaseService.updateTestCaseById(testCaseId, updateTestCaseDto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(EMPTY_INPUT_ERROR);
+    }
+
+    @Test
+    public void TestCaseService_UpdateTestCaseById_ThrowsIllegalArgumentException_WhenExpectedOutputIsBlank(){
+        // Arrange
+        Long testCaseId = 1L;
+        UpdateTestCaseDto updateTestCaseDto = UpdateTestCaseDto.builder()
+                .input("updated input")
+                .expectedOutput(" ") // blank expectedOutput field
+                .isSample(false)
+                .build();
+
+        when(testCaseRepository.findById(testCaseId)).thenReturn(Optional.of(testCase));
+
+        // Act and Assert
+        String EMPTY_EXPECTED_OUTPUT_ERROR = "Cannot set the expectedOutput field to nothing! " +
+                "If you do not wish to update the expectedOutput field, " +
+                "please remove it from the JSON request. Only include what you want to update.";
+        Assertions.assertThatThrownBy(() -> testCaseService.updateTestCaseById(testCaseId, updateTestCaseDto))
+                .isInstanceOf(IllegalArgumentException.class)
+                .hasMessage(EMPTY_EXPECTED_OUTPUT_ERROR);
+    }
+
+    @Test
+    public void TestCaseService_UpdateTestCaseById_ReturnsUpdatedTestCaseDto_WhenInputAndExpectedOutputIsMissing(){
+        // Arrange
+        Long testCaseId = 1L;
+        UpdateTestCaseDto updateTestCaseDto = UpdateTestCaseDto.builder()
+//                .input("updated test case input") commented out input inside the updateDto
+//                .expectedOutput("nothing") commented out expectedOutput inside updateDto
+                .isSample(false)
+                .build();
+
+        TestCase updatedTestCase = TestCase.builder()
+                .input("String s = 'hello world'")
+                .expectedOutput("hello world")
+                .isSample(false)
+                .problem(problem)
+                .build();
+
+        when(testCaseRepository.findById(testCaseId)).thenReturn(Optional.of(testCase));
+        when(testCaseRepository.save(Mockito.any(TestCase.class))).thenReturn(updatedTestCase);
+
+        // Act
+        TestCaseDto result = testCaseService.updateTestCaseById(testCaseId, updateTestCaseDto);
+
+        // Assert
+        Assertions.assertThat(result).isNotNull();
+        Assertions.assertThat(result.getInput()).isEqualTo(updatedTestCase.getInput());
+        Assertions.assertThat(result.getExpectedOutput()).isEqualTo(updatedTestCase.getExpectedOutput());
+        Assertions.assertThat(result.getIsSample()).isFalse();
+    }
+
+    @Test
+    public void TestCaseService_DeleteTestCase_ReturnsSuccessString(){
+        // Arrange
+        Long testCaseId = 1L;
+
+        when(testCaseRepository.findById(testCaseId)).thenReturn(Optional.of(testCase));
+
+        // Act
+        String message = testCaseService.deleteTestCase(testCaseId);
+
+        // Assert
+        Assertions.assertThat(message).isNotBlank().isEqualTo("Test Case deleted successfully (ID): " + testCase);
+    }
+
+    @Test
+    public void TestCaseService_DeleteTestCase_ThrowsTestCaseNotFoundException_WhenNotFound(){
+        // Arrange
+        Long testCaseId = 1L;
+
+        when(testCaseRepository.findById(testCaseId)).thenReturn(Optional.empty());
+
+        // Act and Assert
+        String TESTCASE_NOT_FOUND_ERROR = "Test Case with given ID doesn't exist: ";
+        Assertions.assertThatThrownBy(() -> testCaseService.deleteTestCase(testCaseId))
+                .isInstanceOf(TestCaseNotFoundException.class)
+                .hasMessage(TESTCASE_NOT_FOUND_ERROR + testCaseId);
     }
 }
